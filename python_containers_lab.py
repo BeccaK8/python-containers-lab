8ee8429846fa5bb0645baead84341fcbae86c06d
Rebecca Kiefer <rebeccakiefer8@gmail.com>
2024-01-10 12:44:14 -0600

    exercises 1 and 2

---
+++ python_containers_lab.py
@@ -0,0 +1,15 @@
+# Exercise 1
+print( "\nExercise 1: \n")
+
+students = [ 'Brynlee', 'Lexi', 'Stan', 'Colby', 'Kayla' ]
+print( f"Second student is {students[1]} ")
+print( f"Last student is {students[-1]} ")
+
+
+# Exercise 2
+print( "\nExercise 2: \n")
+
+foods = ( 'Steak', 'Green beans', 'Liver', 'Spaghetti', 'Cookies' )
+for food in foods:
+    print( f"{food} is a good food" )
+
